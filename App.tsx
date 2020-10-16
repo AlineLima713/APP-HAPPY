60ae4feec37324ad8d90899cfadb7b48eb3817e7
Aline Lima <alinelima713@gmail.com>
2020-10-16 19:59:37 -0300

    criação de rotas

--- App.tsx
+++ App.tsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-import mapMarker from './src/images/map-marker.png';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -8.4715781,
-          longitude: -35.7287559,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      />
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
 
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
